42ad218a8f187dc4fe4f894fbefa0a538a7e50db
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-12 22:06:03 +0800

    Made list of links dynamic based on where user is

--- client/src/Components/Navigation/index.js
+++ client/src/Components/Navigation/index.js
@@ -1,13 +1,48 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-export function Navigation() {
-    return(
+import { Link } from 'react-router-dom'
+export function Navigation({ relevantLinks }) {
+    const prepareLinks = () => {
+        if (relevantLinks) {
+            return relevantLinks.map(link => 
+            <li key={link} style={{ margin: "0 1em" }}><Link to={getLink(link)}>{getCaption(link)}</Link></li>);
+        }
+    }
+    const getCaption = (linkName) => {
+        switch (linkName) {
+            case "home":
+                return "Home";
+            case "createLearningStory":
+                return "Create Learning Story";
+            case "signUp":
+                return "SignUp";
+            case "logIn":
+                return "SignIn";
+            case "logOut":
+                return "SignOut";
+            default:
+                return linkName;
+        }
+    }
+    const getLink = (linkName) => {
+        switch (linkName) {
+            case "home":
+                return "/";
+            case "createLearningStory":
+                return "/learningStory";
+            case "signUp":
+                return "/signup";
+            case "logIn":
+                return "/login";
+            case "logOut":
+                return "/logout";
+            default:
+                return linkName;
+        }
+    }
+    return (
         <nav>
-            <ul style={{display: "flex", listStyle: "none"}}>
-                <li style={{margin: "0 1em"}}><Link to="/">Home</Link></li>
-                <li style={{margin: "0 1em"}}><Link to="/signup">Sign Up</Link></li>
-                <li style={{margin: "0 1em"}}><Link to="/login">Log In</Link></li>
-                <li style={{margin: "0 1em"}}><Link to="/logout">Log Out</Link></li>
+            <ul style={{ display: "flex", listStyle: "none" }}>
+                {prepareLinks()}
             </ul>
         </nav>
     )
