e109b16bb73bd8d915d19ede7a1ef036ac4c7bc6
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-14 02:15:18 +0800

    Added processing icon while fetching learning stories for learningStories container

--- client/src/pages/LearningStoriesContainer/index.js
+++ client/src/pages/LearningStoriesContainer/index.js
@@ -3,32 +3,41 @@ import API from '../../utils/API';
 import MessageAlert from "../../Components/MessageAlert";
 import RenderLearningStories from "../../Components/RenderLearningStories";
 import { Container } from "@material-ui/core";
+import CircularIndeterminate from "../../Components/CircularIndeterminate";
 
 
 const LearningStoriesContainer = () => {
     const [learningStories, setLearningStories] = useState();
-    
+
     useEffect(
         () => {
             API.getLearningStories()
-            .then(response => setLearningStories(response.data))
-            .catch(error => console.log(error));
+                .then(response => setLearningStories(response.data))
+                .catch(error => console.log(error));
         }
-    , []);
-    if(learningStories && learningStories.length)
-    {
+        , []);
+    if (learningStories && learningStories.length) {
         return (
-            <RenderLearningStories learningStories={learningStories}/>
+            <RenderLearningStories learningStories={learningStories} />
         )
     }
-    return (
-        <Container>
-           <MessageAlert 
-           message="You have no learning stories to view."
-           severity="info"
-           />
-        </Container>
-    );
+    else if (learningStories && learningStories.length === 0) {
+        return (
+            <Container>
+                <MessageAlert
+                    message="You have no learning stories to view."
+                    severity="info"
+                />
+            </Container>
+        );
+    }
+    else {
+        return (
+            <Container component="main" maxWidth="sm">
+                <CircularIndeterminate />
+            </Container>
+        );
+    }
 }
 
 export default LearningStoriesContainer;
