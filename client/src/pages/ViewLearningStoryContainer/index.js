823cf29ad9a580fc35a3027fd357558a1f4d8f69
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-19 12:25:13 +0800

    Changed student view form background color to honeydew

--- client/src/pages/ViewLearningStoryContainer/index.js
+++ client/src/pages/ViewLearningStoryContainer/index.js
@@ -12,11 +12,12 @@ import { Redirect, useHistory } from "react-router-dom";
 const ViewLearningStoryContainer = ({ learningStoryToView }) => {
     const useStyles = makeStyles((theme) => ({
         root: {
-            background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+            //honeydew
+            background: "linear-gradient(45deg, #F0FFF0 30%, #F0FFF0 90%)",
             border: 0,
             borderRadius: 3,
             boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-            color: "white",
+            color: "black",
             padding: "30px",
         },
         paper: {
